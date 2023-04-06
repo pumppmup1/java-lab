fb9c8a0fd6b545ffe8dbfb9533e5168a87c0e478
pumppmup1 <pumppmup1@gmail.com>
2023-04-06 13:19:30 +0700

    update

---
+++ 007-Min or Max/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		scanner.close();
+		if (a > b) {
+			System.out.println("A");
+		} else if (a < b) {
+			System.out.println("B");
+		} else {
+			System.out.println("AB");
+		}
+	}
+}
