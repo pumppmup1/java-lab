9ff46a328e56b05ce12125df12145fa1811ea065
pumppmup1 <pumppmup1@gmail.com>
2023-04-06 13:06:23 +0700

    update

---
+++ 006-A+B Problem/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		scanner.close();
+		int total = a + b;
+		System.out.println(total);
+	}
+}
