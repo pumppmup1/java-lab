fe648868fec4b8e59be758c1b7681fe5657e4104
pumppmup1 <pumppmup1@gmail.com>
2023-04-06 19:29:54 +0700

    update

---
+++ 010-/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		/*Output Example*/
+		System.out.println("Hello Lab");
+
+		/*Input Example*/
+		Scanner sc = new Scanner(System.in);
+		String stringInput = sc.nextLine();
+
+		/*Output of Input Example*/
+		System.out.println("String Input: " + stringInput);
+	}
+}
+/*Tips:  Run for check input format*/
